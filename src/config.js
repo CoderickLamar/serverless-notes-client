af5adc035a2d1baa7259ec051fdcbc17ceb443c4
Coderick Lamar <codericklamar@sal3000.local>
2020-05-30 09:16:34 -0700

    First commit of our React Notes App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-uploads-sal3000"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://941ruhlgr2.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "1_z2UbrcB1V",
+      APP_CLIENT_ID: "626kd7pud0ldj0tel2t5ecj4g0",
+      IDENTITY_POOL_ID: "us-east-1:6149f379-79ff-492a-b868-76b39862dcf1"
+    }
+};
